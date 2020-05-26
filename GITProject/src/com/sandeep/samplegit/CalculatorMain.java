71226d225ac8efc8d749cf4eeedfbe4a34a8ec13
Sandeep_Thoyakkat <Sandeep_Thoyakkat@ENGLON200120L.ad.infosys.com>
2020-05-26 14:43:41 +0300

    Initial Testing Project for GIT

---
+++ GITProject/src/com/sandeep/samplegit/CalculatorMain.java
@@ -0,0 +1,10 @@
+package com.sandeep.samplegit;
+
+public class CalculatorMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a sample GIT project");
+	}
+
+}
